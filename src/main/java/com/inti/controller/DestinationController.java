d72bb316ac5ec8e795a5cd542483dc61c0eee5b7
landazar <lucaspagies@gmail.com>
2023-06-29 11:33:58 +0200

    Ajout classe, controller et repo pour destination commentaire dans creerHotel

---
+++ src/main/java/com/inti/controller/DestinationController.java
@@ -0,0 +1,63 @@
+package com.inti.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.inti.model.Destination;
+import com.inti.repository.IDestinationRepository;
+
+@Controller
+public class DestinationController {
+	
+	@Autowired
+	IDestinationRepository idr;
+	
+	@GetMapping("creerDestination")
+	public String creerDestination()
+	{
+		return "creerDestination";
+	}
+	
+	@PostMapping("creerDestination")
+	public String creerDestination(@ModelAttribute("destination") Destination d)
+	{		
+		idr.save(d);
+		
+		return "redirect:/listeDestination";
+	}
+	
+	@GetMapping("listeDestination")
+	public String listeDestination(Model m)
+	{
+		m.addAttribute("listeD", idr.findAll());
+		
+		return "listeDestination";
+	}
+	
+	@GetMapping("modifierDestination/{id}")
+	public String modifierDestination(@PathVariable("id") long id, Model m)
+	{
+		m.addAttribute("Destination", idr.getReferenceById(id));
+		return "modifierDestination";
+	}
+	
+	@PostMapping("modifierDestination/{id}")
+	public String modifierDestination(@ModelAttribute("destination") Destination d, @PathVariable("id") long id)
+	{
+		idr.save(d);
+		return "redirect:/listeDestination";
+	}
+	
+	@GetMapping("deleteDestination/{id}")
+	public String deleteDestination(@PathVariable("id") long id)
+	{
+		idr.delete(idr.getReferenceById(id));
+		return "redirect:/listeDestination";
+	}
+
+}
--- src/main/java/com/inti/model/Destination.java
+++ src/main/java/com/inti/model/Destination.java
@@ -1,12 +1,13 @@
 package com.inti.model;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,30 +19,20 @@ import lombok.ToString.Exclude;
 @Entity
 @Table
 @Data @NoArgsConstructor @AllArgsConstructor @RequiredArgsConstructor
-public class Hotel {
+public class Destination {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	@Column(nullable = false)
 	@NonNull
-	private String nom;
-	private int nbEtoile;
+	@Column(nullable = false)
+	private long longitude;
+	@NonNull
+	@Column(nullable = false)
+	private long latitude;
 	
-	public Hotel(@NonNull String nom, int nbEtoile) {
-		super();
-		this.nom = nom;
-		this.nbEtoile = nbEtoile;
-	}
-
-	public Hotel(int nbEtoile) {
-		super();
-		this.nbEtoile = nbEtoile;
-	}
-	
-	@ManyToOne
-	@JoinColumn(name = "id_destination")
+	@OneToMany(mappedBy = "destination")
 	@Exclude
-	private Destination destination;
+	private List<Hotel> listeH;
 
 }
